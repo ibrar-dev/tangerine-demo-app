c9d480e97d8811acfc29e7ee1ae9e976a68c42a9
Muhammad Ibrar <muhammadibrar1@yahoo.com>
2022-05-09 00:18:52 +0500

    Angularjs and Angular10 dual boot

---
+++ src/app/app.module.ts
@@ -0,0 +1,56 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { UpgradeModule, downgradeComponent } from '@angular/upgrade/static';
+import { Ng2DemoComponent } from "./ng2-demo.component";
+import { RouterModule, UrlHandlingStrategy } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { phoneServiceProvider } from './phone.service';
+declare var angular: any;
+
+export class CustomHandlingStrategy implements UrlHandlingStrategy {
+  shouldProcessUrl(url) {
+    return url.toString().startsWith("/ng2-route") || url.toString() == "/"
+  }
+  extract(url) { return url; }
+  merge(url, whole) { return url; }
+}
+``
+angular.module('phonecatApp')
+  .directive(
+    'ng2Demo',
+    downgradeComponent({component: Ng2DemoComponent})
+  );
+@NgModule({
+  declarations: [
+    AppComponent,
+    Ng2DemoComponent
+  ],
+  imports: [
+    BrowserModule,
+    UpgradeModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'ng2-route'
+      },
+      {
+        path: 'ng2-route',
+        component: Ng2DemoComponent
+      }
+    ],
+    {
+      useHash: true,
+      enableTracing: true
+    }
+    )
+  ],
+  providers: [
+    
+    phoneServiceProvider,
+    { provide: UrlHandlingStrategy, useClass: CustomHandlingStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
